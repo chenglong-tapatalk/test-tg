2d375901bca7cae791be6fc26a3f77a26b3696eb
waimailao <wangfurenailaoyang@163.com>
2024-07-10 18:10:57 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
     const [count, setCount] = useState(0)
 
     return (
-    <TonConnectUIProvider manifestUrl="https://<YOUR_APP_URL>/tonconnect-manifest.json">
+    <TonConnectUIProvider manifestUrl="https://test-long.metaforo.io/tonconnect-manifest.json">
         <div>
             <a href="https://vitejs.dev" target="_blank">
                 <img src={viteLogo} className="logo" alt="Vite logo" />
